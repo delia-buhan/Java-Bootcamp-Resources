ff8f9086c1fc3e4482fad2f2fb3688799870656f
Delia Buhan <delia.buhan@gmail.com>
2022-11-27 13:08:53 +0100

    Workshop 1.1 & Challenge 1

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("DDDD   BBBB ");
+         System.out.println("D   D  B   B");
+         System.out.println("D   D  B   B");
+         System.out.println("D   D  BBBB");
+         System.out.println("D   D  B   B");
+         System.out.println("D   D  B   B");
+         System.out.println("DDDD   BBBB");
     }
 }
